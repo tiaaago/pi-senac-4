c580731c6ee2530e7539a5d6626a5c214e4f0b8d
Tiago Enzo <63698660+tiaaago@users.noreply.github.com>
2025-11-02 00:09:25 -0300

    iniciando desenvolvimento da landing-page

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,16 +1,21 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "StudyBuddy — Um novo jeito de estudar!",
-  description: "Um novo jeito de estudar!",
+    title: "StudyBuddy — Um novo jeito de estudar!",
+    description: "Um novo jeito de estudar!",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    );
 }
